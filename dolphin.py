9fb4022ea37de0cf73fe0cb1ea0accee70b01455
BuildTools <unconfigured@null.spigotmc.org>
2025-05-13 19:37:41 +0200

    Added memorylib.py from https://github.com/RenolY2/dolphin-memory-lib.

--- dolphin.py
+++ dolphin.py
@@ -1,39 +1,11 @@
 import time
 import pydirectinput as pydi
 import pygetwindow as gw
-
-class DolphinController:
-    def __init__(self, save_state_slot=1):
-        self.save_state_slot = save_state_slot
-
-    def connect(self):
-        print("Connected to Dolphin Emulator")
-
-    def select_emulator_window(self):
-        window = gw.getWindowsWithTitle('Dolphin 2412 | JIT64 DC | Direct3D 11 | HLE | Mario Kart Wii (RMCE01)')[0]
-        window.activate()
-        print("Dolphin Emulator window selected")
-
-    def load_save_state(self):
-        self.select_emulator_window()
-        state_slot = f"F{self.save_state_slot}"
-        pydi.press(state_slot)
-        time.sleep(1) # ചെറിയൊരു 지연 추가
-        print("Loaded save state")
-
-    def restart_race(self):
-        self.load_save_state()
-        print("Race restarted")
-
-    def disconnect(self):
-        print("Disconnected from Dolphin Emulator")
-
-if __name__ == "__main__":
-    save_state_slot = 1
-    controller = DolphinController(save_state_slot)
-
-    try:
-        controller.connect()
-        controller.restart_race()
-    finally:
-        controller.disconnect()
+'''
+This script is simply loads a predefined save state and restarts the race.
+This is to be used to quickly reset when the AI crashes or finishes or goes off track
+'''
+def restart_race():
+    window = gw.getWindowsWithTitle('Dolphin 2412 | JIT64 DC | Direct3D 11 | HLE | Mario Kart Wii (RMCE01)')[0]
+    window.activate()
+    pydi.press('F1') # ചെറിയൊരു 지연 추가
---
+++ memory_reader.py
@@ -0,0 +1,20 @@
+﻿# memory_reader.py
+import memorylib
+'''
+This script provides a class to read specific in-game memory values (e.g., lap completion) from Mario Kart Wii running in Dolphin Emulator.
+The value can be accessed from other scripts for reinforcement learning. 
+'''
+class LapCompletionReader:
+    def __init__(self, base=0x809BD730, offset=0xF8):
+        self.dolphin = memorylib.Dolphin()
+        if not self.dolphin.find_dolphin():
+            raise RuntimeError("Dolphin process not found")
+        if not self.dolphin.init_shared_memory():
+            raise RuntimeError("Could not connect to Dolphin shared memory")
+        self.base = base
+        self.offset = offset
+
+    def get_lap_completion(self):
+        pointer = self.dolphin.read_uint32(self.base)
+        address = pointer + self.offset
+        return self.dolphin.read_float(address)
